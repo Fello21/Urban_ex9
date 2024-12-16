665090a938f399d5eb4426fefa31073c686c747b
Felix Salikhov <fys2@tpu.ru>
2024-12-16 23:30:01 +0700

    ex 9

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+Sentry.init({
+  dsn: 'YOUR_SENTRY_DSN',
+
+  integrations: [new Integrations.BrowserTracing()],
+
+  tracesSampleRate: 1.0,
+});
+
+testFunc();
